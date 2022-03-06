a7977651bccfb359eb1857af126b2b84ebc451c0
Siddhant <santoshshrivastava39@gmail.com>
2022-03-06 13:51:38 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    /// <summary>
+    /// Contains the Main Method
+    /// </summary>
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+        }
+    }
+}
